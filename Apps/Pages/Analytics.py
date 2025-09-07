239b86c6928f2733ef20180c51fe4e53e206d0da
Avanindra Bose <avanindrabose0218@gmail.com>
2025-09-07 13:52:36 +0530

    Analytics Module Completed

--- Apps/Pages/Analytics.py
+++ Apps/Pages/Analytics.py
@@ -1,4 +1,107 @@
 import streamlit as st 
 import pickle 
+import pandas as pd 
+import numpy as np 
+import plotly.express as px
+from wordcloud import WordCloud 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.set_page_config(page_title='Analytics')
+
+new_df = pd.read_csv('Cleaned Datasets/data_viz1.csv')
+
+with open('Apps/feature_text.pkl','rb') as model :
+    features_text = pickle.load(model)
+# st.dataframe(new_df)
+
+group_df = new_df.groupby('sector').mean(numeric_only=True)[['price','price_per_square_feet','built_up_area','latitude','longitude']]
+
+group_df[['price','price_per_square_feet','built_up_area']] = group_df[['price','price_per_square_feet','built_up_area']].round(2)
+st.header('Sector Price per Sqft Geomap')
+fig = px.scatter_map(group_df, lat="latitude", lon="longitude", color="price_per_square_feet", size='built_up_area',
+                  color_continuous_scale=px.colors.cyclical.IceFire, zoom=10,width = 1200 ,height=700 , hover_name = group_df.index , 
+                  map_style="open-street-map")
+
+st.plotly_chart(fig,use_container_width=True)
+
+st.header('Facilities Provided')
+wordcloud = WordCloud(
+    width=800,
+    height=800,
+    background_color='white',
+    stopwords=set(['s']),
+    min_font_size=10
+).generate(features_text)
+
+# Create figure
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis("off")
+
+# Display in Streamlit
+st.pyplot(fig)
+
+st.header('Area Vs Price')
+
+property_type = st.selectbox('Select Property Type' , ['flat' , 'house'])
+
+
+if property_type == 'house':
+    fig1 = px.scatter(new_df[new_df['property_type'] == 'house'], x="built_up_area", y="price", color="bedRoom", title="Area Vs Price")
+
+    st.plotly_chart(fig1, use_container_width=True)
+else:
+    fig1 = px.scatter(new_df[new_df['property_type'] == 'flat'], x="built_up_area", y="price", color="bedRoom",
+                      title="Area Vs Price")
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+st.header('BHK Pie Chart')
+
+sector_options = new_df['sector'].unique().tolist()
+sector_options.insert(0,'overall')
+
+selected_sector = st.selectbox('Select Sector', sector_options)
+
+if selected_sector == 'overall':
+
+    fig2 = px.pie(new_df, names='bedRoom')
+
+    st.plotly_chart(fig2, use_container_width=True)
+else:
+
+    fig2 = px.pie(new_df[new_df['sector'] == selected_sector], names='bedRoom')
+
+    st.plotly_chart(fig2, use_container_width=True)
+
+st.header('Side by Side BHK price comparison')
+
+fig3 = px.box(new_df[new_df['bedRoom'] <= 4], x='bedRoom', y='price', title='BHK Price Range' ,labels={'price' : 'Price in Cr' , 'bedRoom' : 'Number of Bedrooms'})
+st.plotly_chart(fig3, use_container_width=True)
+
+st.header('KDE plot for property type')
+
+fig3, ax = plt.subplots(figsize=(10, 4))
+
+sns.kdeplot(new_df[new_df['property_type'] == 'house']['price'], 
+            label='House', color='blue', ax=ax , fill=True)
+
+sns.kdeplot(new_df[new_df['property_type'] == 'flat']['price'], 
+            label='Flat', color='orange', ax=ax , fill = True)
+
+ax.legend()
+ax.set_xlabel("Price")
+ax.set_ylabel("Density")
+
+st.pyplot(fig3)
+
+
+
+
+
+
+
+
+
+
